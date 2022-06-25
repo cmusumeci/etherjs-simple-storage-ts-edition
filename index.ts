902bcfbb9ad5c51b42e2db911a64b203b9990ed1
cfmusumeci <cmusumeci89@gmail.com>
2022-06-25 20:14:56 +0200

    feat: Improvements

--- index.ts
+++ index.ts
@@ -1,1 +1,39 @@
-console.log("cacca");
+import { ethers } from "ethers";
+import { readFile } from "fs/promises";
+import { join } from "path";
+const ABI_FILE = join(__dirname, "SimpleStorage_sol_SimpleStorage.abi");
+const BINARY_FILE = join(__dirname, "SimpleStorage_sol_SimpleStorage.bin");
+
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545" // GANACHE
+  );
+  const wallet = new ethers.Wallet(
+    "078f073263088decf6b01cab71faabe8436ae4f61b42379359d992809994ba52", // PRIVATE KEY
+    provider
+  );
+  console.log(ABI_FILE);
+  try {
+    const abi = await readFile(ABI_FILE, {
+      encoding: "utf-8",
+    });
+
+    const binary = await readFile(BINARY_FILE, {
+      encoding: "utf-8",
+    });
+
+    const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+    console.log("Deploying, wait....");
+    const contract = await contractFactory.deploy();
+    console.log(contract);
+  } catch (error) {
+    throw error;
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
